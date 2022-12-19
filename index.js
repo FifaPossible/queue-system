dccd346c29ce38b3a2b237c365773e3aa1134428
FifaPossible <65070528+FifaPossible@users.noreply.github.com>
2022-12-19 12:18:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -52,6 +52,6 @@ app.use(flash());
 
 app.use(router);
 
-app.listen(process.env.PORT, () =>
+app.listen(process.env.PORT || 3000, () =>
    console.log(`app started in port ${process.env.PORT}`)
 );
